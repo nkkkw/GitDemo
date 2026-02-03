17007673a9bcbe73d42f08b8bec14906414e09b2
nkkkw <nkkkw1205@163.com>
2026-02-03 21:03:47 +0800

    学生创建了

---
+++ src/main/java/com/nkkkw/gittest/Student.java
@@ -0,0 +1,10 @@
+package com.nkkkw.gittest;
+
+public class Student {
+    int age;
+    String name;
+    public Student(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+}
